8ed55f03198d2e5dac4b5cb88aa7dc5beb207579
Baljeetsinghcode <baljeet.singh@codequotient.com>
2023-05-11 12:02:30 +0530

    11.5.2023

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app=express();
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
 
+
 const client=require("mongodb").MongoClient;
+const Objid=require("mongodb").ObjectId;
+
 //(err,database)
 //catch,then
 let dbinstance;
@@ -35,5 +38,37 @@ app.post("/storeData",(req,res)=>{
         //res.render("home",{data:result});
         res.redirect("/getData");
     })
+})
+app.get("/viewData/:id",(req,res)=>{
+
+    //console.log(req.params.id);
+    dbinstance.collection("Students").findOne({"_id":new Objid(req.params.id)}).then(result=>{
+
+        res.render("Student",{data:result});
+
+    });
+
+   
+
+
+
+})
+
+app.get("/updateData/:id",(req,res)=>{
+
+    //console.log(req.params.id);
+    dbinstance.collection("Students").findOne({"_id":new Objid(req.params.id)}).then(result=>{
+
+        res.render("UpdateStudent",{data:result});
+
+    });
+});
+app.post("/updateData",(req,res)=>{
+//update Students set Name=req.body.name,age=req.body.age where _id=req.body.id
+
+    dbinstance.collection("Students").updateOne({"_id":new Objid(req.body.id)},{$set:{"name":req.body.name,"age":req.body.age}});
+    res.redirect("/getData");
+
+
 })
 app.listen(3000);
